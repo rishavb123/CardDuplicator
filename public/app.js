f3cba7fe232e95485ed0bff5d12a8f5da8b5447d
Rishav Bhagat <rishavb123@gmail.com>
2018-08-21 11:48:49 -0400

    connected firebase

---
+++ public/app.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+let config = {
+    apiKey: "AIzaSyBG8Vxk1MsP2bEZsOpe4cJYrIbqkGbFHZE",
+    authDomain: "card-duplicator.firebaseapp.com",
+    databaseURL: "https://card-duplicator.firebaseio.com",
+    projectId: "card-duplicator",
+    storageBucket: "card-duplicator.appspot.com",
+    messagingSenderId: "70748670873"
+};
+firebase.initializeApp(config);
