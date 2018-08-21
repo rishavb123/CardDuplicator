332edecdbc18b4f76e3b7671f494653c416b902a
Rishav Bhagat <rishavb123@gmail.com>
2018-08-21 12:02:51 -0400

    allowed file upload to firebase storage

--- public/app.js
+++ public/app.js
@@ -8,3 +8,36 @@ let config = {
     messagingSenderId: "70748670873"
 };
 firebase.initializeApp(config);
+
+let uploader = document.getElementById('progress');
+let fileButton = document.getElementById('cameraInput');
+
+fileButton.addEventListener('change', e => {
+
+    let file = e.target.files[0];
+
+    let storageRef = firebase.storage().ref('photos/' + file.name);
+
+    let task = storageRef.put(file);
+
+    task.on('state_changed',
+
+        function progress(snapshot)
+        {
+            let percent = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+            uploader.value = percent;
+        },
+
+        function error(err)
+        {
+
+        },
+
+        function complete()
+        {
+
+        }
+
+    );
+
+});
