54f01abb4b9f09688ce01492ceb59706c8036c55
Rishav Bhagat <rishavb123@gmail.com>
2018-08-22 22:30:01 -0400

    created loop

--- server/server.py
+++ server/server.py
@@ -3,6 +3,7 @@ from firebase_admin import credentials, storage, db
 import numpy as np
 import cv2
 from skimage import io
+from scipy import ndimage
 
 cred = credentials.Certificate("credentials\cred.json")
 app = firebase_admin.initialize_app(cred, {
@@ -17,18 +18,21 @@ bucket = storage.bucket()
 
 ref = db.reference('job')
 
-def getImageExplained():
-    b = bucket.blob("photos/" + s + " - image.jpg")
-    url = bucket.blob("photos/" + s + " - image.jpg").public_url
-    image_read = io.imread(url)
-    image_colors_fixed = cv2.cvtColor(image_read, cv2.COLOR_BGR2RGB)
-    image_resized = cv2.resize(image_colors_fixed, (0, 0), fx=0.3, fy=0.3)
-    return image_resized
+# def getImageExplained():
+#     b = bucket.blob("photos/" + s + " - image.jpg")
+#     url = bucket.blob("photos/" + s + " - image.jpg").public_url
+#     image_read = io.imread(url)
+#     image_colors_fixed = cv2.cvtColor(image_read, cv2.COLOR_BGR2RGB)
+#     image_resized = cv2.resize(image_colors_fixed, (0, 0), fx=0.3, fy=0.3)
+#     rotated =  ndimage.rotate(image_resized, -90)
 
 def getImage():
-    return cv2.resize(cv2.cvtColor(io.imread(bucket.blob("image.jpg").public_url), cv2.COLOR_BGR2RGB), (0, 0), fx=0.3, fy=0.3)
+    return ndimage.rotate(cv2.resize(cv2.cvtColor(io.imread(bucket.blob("image.jpg").public_url), cv2.COLOR_BGR2RGB), (0, 0), fx=0.3, fy=0.3), -90)
 
-cv2.imshow("image", getImage())
+while True:
+    
+    cv2.imshow("image", getImage())
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
 
-cv2.waitKey(0)
 cv2.destroyAllWindows()
